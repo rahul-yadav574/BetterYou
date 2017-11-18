eba06e029a1272ccbf4e760314f0e19870c74bc3
rahulyadav20111995 <rahulyadav574@gmail.com>
2017-11-18 17:40:57 +0530

    added pdf js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('views',path.join(__dirname,'pdfjs/web'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pdfjs')));
+app.use(express.static(path.join(__dirname, 'pdfjs/web')));
 
 app.use('/', index);
 app.use('/users', users);
